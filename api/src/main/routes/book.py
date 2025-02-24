c80c412ccd989a11dbbf317a3675a54513298e9d
Estefany Reis <estefanyreissregis@gmail.com>
2025-02-24 18:13:07 -0300

    feat: adciona request e response na rota

--- api/src/main/routes/book.py
+++ api/src/main/routes/book.py
@@ -1,8 +1,16 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+
+from src.http_types.http_request import HttpRequest
+from src.http_types.http_response import HttpResponse
 
 
 book_route_bp = Blueprint("book_route", __name__)
 
 @book_route_bp.route("/livros", methods = ["POST"] )
 def create_new_book():
-    return jsonify({"estou" : "aqui"}), 201
+    
+    http_request = HttpRequest(body=request.json)
+    
+    http_response = HttpResponse(body=http_request.body, status_code=201)
+    
+    return jsonify(http_response.body), http_response.status_code
